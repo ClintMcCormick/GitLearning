d0013cc46887870b0b4eadff9fc6410ae8d3e868
ClintMcCormick <william.mccormick@levvel.io>
2021-10-02 10:35:12 -0400

    creating repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n You better get good at this because you will use it for the rest of your career!");
+    }
+}

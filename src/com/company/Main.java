9a945927a5281e8bf1ecfbd358840c460430f180
ClintMcCormick <william.mccormick@levvel.io>
2021-10-02 10:45:38 -0400

    Dev Branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("\n You better get good at this because you will use it for the rest of your career!");
+        System.out.println("\n You better get good at this because you will use it for the rest of your career!\n");
+        System.out.println("\n This is the new Develop branch!\n");
     }
 }
